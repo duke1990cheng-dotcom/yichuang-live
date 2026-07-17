e7eba181506caa4b03888f1b2743053cfd39d1ce
Duke <duke@example.com>
2026-07-17 10:27:50 +0800

    content: publish baike 004

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/articles/does-heat-film-affect-light",
+        destination: "/articles/baike-004-window-film-daylighting",
+        permanent: true
+      }
+    ];
+  }
+};
 
 export default nextConfig;
