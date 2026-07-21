7ddfb0fa849b6d0e3d6ccf8a6acc067a59daf459
Duke <duke@example.com>
2026-07-21 10:13:05 +0800

    content: publish baike 007-008

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
         source: "/articles/does-heat-film-affect-light",
         destination: "/articles/baike-004-window-film-daylighting",
         permanent: true
+      },
+      {
+        source: "/articles/home-window-film-vs-car-film",
+        destination: "/articles/baike-008-home-window-film-vs-car-film",
+        permanent: true
       }
     ];
   }
